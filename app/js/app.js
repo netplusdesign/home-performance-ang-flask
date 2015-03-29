c8624d78debc1afce93dc48790ef307e7b9b52a7
Larry Burks <lburks@gmail.com>
2015-03-29 14:03:39 -0400

    point to static directory

--- app/js/app.js
+++ app/js/app.js
@@ -18,31 +18,31 @@ config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 		when ( '/yearly/:view', { 
 			templateUrl: function ( params ) { 
-				return 'partials/yearly/' + params.view + '.html'; 
+				return 'static/partials/yearly/' + params.view + '.html'; 
 			}, 
 			controller: 'YearlyCtrl'
 		}).
 		when ( '/monthly/:view', { 
 			templateUrl: function ( params ) { 
-				return 'partials/monthly/' + params.view + '.html'; 
+				return 'static/partials/monthly/' + params.view + '.html'; 
 			}, 
 			controller: 'MonthlyCtrl'
 		}).
 		when ( '/monthly/:view/:circuit', { 
 			templateUrl: function ( params ) { 
-				return 'partials/monthly/' + params.view + '.html'; 
+				return 'static/partials/monthly/' + params.view + '.html'; 
 			}, 
 			controller: 'MonthlyCtrl'
 		}).
 		when ( '/daily/:view', { 
 			templateUrl: function () { 
-				return 'partials/daily.html'; 
+				return 'static/partials/daily.html'; 
 			}, 
 			controller: 'DailyCtrl'
 		}).
 		when ( '/daily/:view/:circuit', { 
 			templateUrl: function () { 
-				return 'partials/daily.html'; 
+				return 'static/partials/daily.html'; 
 			}, 
 			controller: 'DailyCtrl'
 		}).
