b18d46ae1e8c30de1ddf14ee9ee981626bcc6aa4
Larry Burks <lburks@gmail.com>
2016-12-05 09:08:15 -0500

    add new hourly controller, views and tests

---
+++ app/hourly/hourly.controller.js
@@ -0,0 +1,249 @@
+'use strict';
+
+/* Controllers */
+
+/* global angular */
+/* global moment */
+/* jshint strict : true */
+/* jshint undef : true */
+/* jshint unused : true */
+/* jshint globalstrict : true */
+
+angular.module('myApp.controllers.hourly', []).
+
+	controller( 'HourlyCtrl', [
+		'$scope',
+		'$routeParams',
+		'dataProvider',
+		'metadataService',
+		'dataService',
+		'chartService',
+		function (
+			$scope,
+			$routeParams,
+			dataProvider,
+			metadataService,
+			dataService,
+			chartService
+		) {
+
+			var showSummary = function ( data ) {
+
+				if ( typeof data.items[0] === 'undefined' ) {
+
+					$scope.warning = true;
+				} else {
+
+					$scope.data = data;
+				}
+			},
+
+			showGeneration = function ( data ) {
+
+				if ( typeof data.items === 'undefined' ) {
+
+					$scope.warning = true;
+				}
+				else {
+
+					data.max_solar_hour.date = moment( data.max_solar_hour.date ).toDate();
+
+					$scope.data = data;
+				}
+			},
+
+			showUsage = function ( data ) {
+
+				switch ( data.circuit.circuit_id ) {
+
+					case 'summary' :
+
+						if ( data.circuits[0].actual === null ) {
+
+							$scope.warning = true;
+						}
+						else {
+
+							if (data.circuits[0].circuit_id == 'used') { data.circuits[0].circuit_id = 'all'; } // shim
+
+							$scope.data = dataService.insertPercent ( data, 'circuits', 'actual' );
+						}
+
+						break;
+
+					case 'all' :
+
+						if ( typeof data.items === 'undefined' ) {
+
+							$scope.warning = true;
+						}
+						else {
+
+							$scope.data = dataService.insertDiff ( data, 'budget', 'actual' );
+
+							$routeParams.view = 'circuit';
+						}
+
+						break;
+
+					case 'ashp' :
+
+						if ( typeof data.items === 'undefined' ) {
+
+							$scope.warning = true;
+						}
+						else {
+
+							$scope.data = data;
+
+							$routeParams.view = 'circuit';
+						}
+
+						break;
+
+					default :
+
+						if ( typeof data.items === 'undefined' ) {
+
+							$scope.warning = true;
+						}
+						else {
+
+							$scope.data = data;
+
+							$routeParams.view = 'circuit';
+						}
+				}
+
+			},
+
+			showTemperature = function ( data ) {
+				// fix max_solar_hour time by parsing text into a date
+				if ( typeof data.items === 'undefined' ) {
+
+					$scope.warning = true;
+				}
+				else {
+
+					data.location_name = metadataService.locations[ data.location ];
+
+					$scope.data = data;
+				}
+			},
+
+			showWater = function ( data ) {
+
+				if ( typeof data.items === 'undefined' ) {
+
+					$scope.warning = true;
+				}
+				else {
+
+					$scope.data = dataService.insertEfficiency ( data );
+
+					$scope.data = dataService.insertAverage (data, ['cold', 'hot', 'main'], ['cold_avg', 'hot_avg', 'main_avg']);
+				}
+			},
+
+			showBasetemp = function ( data ) {
+
+				if ( typeof data.points === 'undefined' ) {
+
+					$scope.warning = true;
+				}
+				else {
+
+					$scope.data = dataService.insertLinearRegression ( data );
+				}
+			},
+
+			setOptionsIfBasetemp = function ( params ) {
+
+				var options = {};
+
+				if ( params.view == 'basetemp' ) {
+
+					if ( params.interval !== undefined ) {
+
+						options.interval = params.interval;
+					}
+					else {
+
+						options.interval = 'hours';
+					}
+					if ( params.base !== undefined ) {
+
+						options.base = params.base;
+					}
+					else {
+
+						options.base = 65;
+					}
+				}
+				return options;
+			},
+
+			setRouteParams = function ( params ) {
+
+				if ( params.view == 'basetemp' ) {
+
+					$routeParams.interval = $scope.options.interval;
+
+					$routeParams.base = $scope.options.base;
+				}
+				if ( (params.view == 'usage') && (params.filter == 'ashp') ) {
+
+					$routeParams.base = metadataService.basetemp.base;
+				}
+				return $routeParams;
+			};
+
+			$scope.warning = false;
+
+			$routeParams.path = 'hours';
+
+			$scope.options = setOptionsIfBasetemp ( $routeParams );
+
+			$scope.update = function () {
+
+				$routeParams = setRouteParams ( $routeParams );
+
+				dataProvider.getDailyData( $routeParams ).then( function( data ) {
+
+					switch ( $routeParams.view ) {
+
+						case 'summary' : showSummary ( data ); break;
+
+						case 'generation' : showGeneration ( data ); break;
+
+						case 'usage' : showUsage ( data ); break;
+
+						case 'temperature' : showTemperature ( data ); break;
+
+						case 'water' : showWater ( data ); break;
+
+						case 'basetemp' : showBasetemp ( data );
+					}
+					// show warnings if no data returned
+					if ( $scope.warning ) {
+
+						$scope.message = "Oops, you've asked for a house, year or interval that is not supported.";
+					}
+					else {
+						// this is the only place metadataService is used in this controller, can get this from data?
+						$scope.year = metadataService.current.year;
+						$scope.house = metadataService.data.houseId;
+						$scope.date = metadataService.data.chartDate; // used for usage screens only
+						// send data to chartService
+						chartService.setData ( $routeParams.view, data );
+					}
+				}, function ( reason ) {
+
+					$scope.warning = true;
+
+					$scope.message = reason;
+				});
+
+			}; $scope.update();
+
+	}]);
